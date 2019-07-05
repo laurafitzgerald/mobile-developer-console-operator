edfd51cff10266a2a928ea312068d3433b718d84
Ali Ok <aliok@redhat.com>
2019-07-05 13:30:42 +0300

    Addressing some comments

--- pkg/controller/mobiledeveloperconsole/mobiledeveloperconsole.go
+++ pkg/controller/mobiledeveloperconsole/mobiledeveloperconsole.go
@@ -237,7 +237,6 @@ func newMDCDeploymentConfig(cr *mdcv1alpha1.MobileDeveloperConsole) (*openshifta
 								fmt.Sprintf("--client-secret=%s", cr.Spec.OAuthClientSecret),
 								"--upstream=http://localhost:4000",
 								"--http-address=0.0.0.0:4180",
-								"--skip-auth-regex=/rest/sender,/rest/registry/device,/rest/prometheus/metrics,/rest/auth/config",
 								"--https-address=",
 								fmt.Sprintf("--cookie-secret=%s", cookieSecret),
 								"--cookie-httponly=false", // we kill the possibility to run MDC on a http route
