3f6e6afe8dcc66c561321e87770e505f05d6507f
Laura Fitzgerald <lfitzger@redhat.com>
2019-10-07 12:56:54 +0100

    create the watch and cr when a crd is created after deployment for Service Monitor

---
+++ pkg/util/state_manager.go
@@ -0,0 +1,31 @@
+package util
+
+import "sync"
+
+type stateManager struct {
+	*sync.Mutex
+	state map[string]interface{}
+}
+
+var singleton *stateManager
+var once sync.Once
+
+func GetStateManager() *stateManager {
+	once.Do(func() {
+		singleton = &stateManager{Mutex: &sync.Mutex{}}
+		singleton.state = make(map[string]interface{})
+	})
+	return singleton
+}
+
+func (sm *stateManager) GetState(key string) interface{} {
+	sm.Lock()
+	defer sm.Unlock()
+	return sm.state[key]
+}
+
+func (sm *stateManager) SetState(key string, value interface{}) {
+	sm.Lock()
+	defer sm.Unlock()
+	sm.state[key] = value
+}
